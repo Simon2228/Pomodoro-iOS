a618240cc5c5fce15152b81be33c87da04b26884
Zhixing Meng <z3meng@ucsd.edu>
2015-04-10 20:44:08 -0700

    created screens

---
+++ Pomodoro/Pomodoro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by Simon Meng on 4/10/15.
+//  Copyright (c) 2015 Simon Meng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
